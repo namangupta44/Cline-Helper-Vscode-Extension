4aaeb1736df4e4b3c3a9824eab391958bc025778
namangupta44 <namank44.44@gmail.com>
2025-11-01 16:40:41 +0530

    updated header to have icons for navigation

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -2,25 +2,14 @@ import {
   VSCodePanels,
   VSCodePanelTab,
   VSCodePanelView,
-  VSCodeButton,
 } from '@vscode/webview-ui-toolkit/react';
 import { ListOpenFiles } from './features/listOpenFiles/ListOpenFiles';
 import { FileNameSearcher } from './features/fileNameSearcher/FileNameSearcher';
 import { FileAndFolderCollector } from './features/fileAndFolderCollector/FileAndFolderCollector';
-import { vscode } from './platform/vscode';
 
 function App() {
-  const handleOpenInEditor = () => {
-    vscode.postMessage({ type: 'openInEditor' });
-  };
-
   return (
     <main>
-      <header className="app-header">
-        <VSCodeButton onClick={handleOpenInEditor} appearance="secondary">
-          Open in Editor
-        </VSCodeButton>
-      </header>
       <VSCodePanels>
         <VSCodePanelTab id="tab-1">OPEN FILES</VSCodePanelTab>
         <VSCodePanelTab id="tab-2">SEARCHER</VSCodePanelTab>
